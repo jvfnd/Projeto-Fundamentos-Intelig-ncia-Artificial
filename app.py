82c547c2f9f9385475c877fae2bb09a3c643fa1c
João Vitor <113381115+jvfnd@users.noreply.github.com>
2025-06-15 17:56:04 -0300

    Criando App.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from google import generativeai as genai
+
+genai.configure(api_key="AIzaSyC0higEx0RP0rl_BNDX0R9mMfpmqHqRsqk")
+model = genai.GenerativeModel("gemini-2.0-flash")
+
+def analise_texto(texto):
+    prompt = f"Analise o seguinte texto e forneça um resumo: {texto}"
+    response = model.generate_content(prompt)
+    return response.text
+
+texto = st.text_area("Cole o texto aqui para a IA analisar:")
+
+
+if st.button("Analisar Texto"):
+    if texto:
+        st.write("Texto analisado com sucesso!")
+        resultado = analise_texto(texto)
+        st.write("Resultado da análise:", resultado)
+    else:
+        st.error("Por favor, cole um texto antes de analisar.")
